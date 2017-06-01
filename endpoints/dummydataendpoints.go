dea94196ae5ab1d62e2d7ce3cb34908d398bf430
greenac <andre1226.dev@gmail.com>
2017-06-01 09:40:42 -0700

    Updated all imports for kapitol repos

--- endpoints/dummydataendpoints.go
+++ endpoints/dummydataendpoints.go
@@ -2,8 +2,8 @@ package endpoints
 
 import (
 	"net/http"
-	"github.com/greenac/testserver/restresponse"
-	"github.com/greenac/testserver/models"
+	"github.com/kapitol-app/testserver/restresponse"
+	"github.com/kapitol-app/testserver/models"
 	"fmt"
 )
 
