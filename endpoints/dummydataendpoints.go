00ac74d3e59e0574ed9fca2fde275afe3940719b
greenac <andre1226.dev@gmail.com>
2017-06-01 10:35:47 -0700

    Added abs path to file to fix read error

--- endpoints/dummydataendpoints.go
+++ endpoints/dummydataendpoints.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"github.com/kapitol-app/testserver/restresponse"
 	"github.com/kapitol-app/testserver/models"
-	"fmt"
 )
 
 type DummyDataEndpoints struct {}
@@ -15,7 +14,6 @@ func (ddep *DummyDataEndpoints)GetSenatorUri() string {
 
 func (ddep *DummyDataEndpoints)GetSenators(w http.ResponseWriter, senators *[]models.Senator) {
 	sens := make([]map[string]interface{}, 0, len(*senators))
-	fmt.Println("senators:", sens)
 	for _, senator := range *senators {
 		sens = append(sens, senator.ToMap())
 	}
